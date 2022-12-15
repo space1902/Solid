1fa43c0c8b5e6275493d95695f6b51b8ee8a7dd7
victoroe <victor.ortizes@pragma.com.co>
2022-12-14 19:08:48 -0500

    ejemplo 2

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,45 @@
+(() => {
+
+    const arrayTemperatureCelsius = [33.6, 12.34];
+
+    const ipServer = '123.123.123.123';
+
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const userEmails = users.map( user => user.email );
+
+    const isJump = false;
+    const isRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+    const startTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    const endTime = new Date().getTime() - startTime;
+
+
+    // Funciones
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBooksByUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+    function getAreaCuadrado( side: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
